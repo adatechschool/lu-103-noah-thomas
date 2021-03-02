c284d7fd48c17ba59439a1497c0ff7a9d1f0f3c0
tob-0 <tbsx@outlook.fr>
2021-03-02 10:17:41 +0100

    [refacto!][Comprendre/&(*quer)/] moved to async/

--- Pratiquer/src/codin-game/codin-game-001.js
+++
@@ -1,7 +0,0 @@
-function job() {
-    return new Promise(function(resolve,reject){
-        setTimeout(()=>{resolve('hello world')},2000)
-    })
-}
-
-module.exports = job;
--- Pratiquer/src/fetch.js
+++
@@ -1,4 +0,0 @@
-const fetch = require('node-fetch');
-fetch('https://www.affirmations.dev/')
-.then(response => response.json())
-.then(data => console.log(data))
