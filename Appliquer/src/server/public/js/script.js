ffc05fa4e33e3b4a0f46a4cd054ae411f56cd330
Thomas 'tob' Blochet <tbsx@outlook.fr>
2021-03-02 01:34:18 +0100

    (almost) Handling everything

--- Appliquer/src/server/public/js/script.js
+++ Appliquer/src/server/public/js/script.js
@@ -1,38 +1,52 @@
-var getQuoteFromWeb = (url,selector) => {
+var getQuote = (url,selector) => {
     fetch(url)
-    .then(response => response.json())
+    .then(response =>{
+        try {
+            return response.json()
+        } catch (error) {
+            if (error instanceof SyntaxError){
+            } else {
+                throw error
+            }
+        }
+    })
     .then(data => {
+        let quote;
+        let author;
         if (selector === 0) {
             quote = data.affirmation
             author = "me, to you"
         } else if (selector === 1) {
             quote = data.data[0].quote
             author = data.data[0].author
+        } else {
+            quote = data.quote
+            author = data.author
         }
+        console.log(quote);
+        console.log(author);
         quote_box.innerHTML = quote
         quote_author.innerHTML = author
     });
 }
 
-var getQuoteFromLocal = _ =>{
-    fetch("/data/db.json")
-}
-
-
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
+
 
 var quote_box = document.getElementById('quote')
 var quote_author = document.getElementById('quote-author')
 
 var selector = getRandomInt(3)
-
+var url;
+open
 if (selector === 0){
-    getQuoteFromWeb('https://www.affirmations.dev/',selector)
+    url = 'https://www.affirmations.dev/'
 } else if (selector === 1){
-    getQuoteFromWeb('http://selfconfidencequotes.herokuapp.com/',selector)
+    url = 'http://selfconfidencequotes.herokuapp.com/'
 } else {
-    getQuoteFromLocal()
+    url = '/data/db.json'
 }
 
+getQuote(url,selector)
